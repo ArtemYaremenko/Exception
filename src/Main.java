beb97172ff61583601ecb162638eed5a361ac1d2
Artem <artem.yaremenko.1992@gmail.com>
2024-08-11 20:36:15 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+public static void authenticateUser(String login, String password, String repeatPassword){
+
+}
